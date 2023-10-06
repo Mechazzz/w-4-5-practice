1a91d4c747c469f65d5d2fa12a4947efdc968d89
Mechazzz <129221431+Mechazzz@users.noreply.github.com>
2023-10-06 23:46:47 +0200

    update

--- script.js
+++ script.js
@@ -1,35 +1,68 @@
-const arr = [1, 2, 3, 4, 5]
+const studentsData = [
+    {
+        name: "Kovács János",
+        age: 29,
+        subjects: [
+            {
+                subjectName: "Maths",
+                subjectGrade: 5
+            },
+            {
+                subjectName:"Physics",
+                subjectGrade: 3
+            },
+            {
+                subjectName: "English",
+                subjectGrade: 2
+            }
+        ]
+    },
+    {
+        name: "Kis Péter",
+        age: 29,
+        subjects: [
+            {
+                subjectName: "Maths",
+                subjectGrade: 2
+            },
+            {
+                subjectName:"Physics",
+                subjectGrade: 5
+            },
+            {
+                subjectName: "English",
+                subjectGrade: 4
+            }
+        ]
+    },
+    {
+        name: "Leföld Elek",
+        age: 95,
+        subjects: [
+            {
+                subjectName: "Maths",
+                subjectGrade: 2
+            },
+            {
+                subjectName:"Physics",
+                subjectGrade: 1
+            },
+            {
+                subjectName: "English",
+                subjectGrade: 5
+            }
+        ]
+    }
+]
 
-function returnDouble(number) {
-    return number * 2
-}
-
-// arrow function es hagyomanyos function kozott a kinezet a kulonbseg ( ha egy parameterem van csak, elhagyhatom a zarojelet, illetve ha egy sorban van az egesz elhagyhatom a return szot es a kapcsos zarojelet)
-/*  const returnDouble2 = (number) => {
-    return number * 2 
-}  */
-
- const returnDouble2 = number => number * 2
-
- console.log(returnDouble(25))
-
-for (let i=0; i < arr.length; i++) {
-   console.log(returnDouble(arr[i]))  // meghivom a fuggvenyt es atadom az array i edik elemét
-} 
-
-
-/*  function logDouble(number) {
-    console.log(number * 2)
+/* for (let i = 0; i <DataTransfer.length; i++){
+console.log(data[i].name)
 } */
 
-// olyan fuggveny ami az arrayen van, ezt az arrayen kell lefuttatni: (a metodus az az olyan fuggveny ami mast adatokon el)
-/* arr.forEach(function(element){
-    console.log(element*2)
-}) */
+/* data.forEach(student => console.log(student.name)) */
 
-
-/* arr.forEach(logDouble) */
-
-//inline fuggveny amikor nincs kiszervezve hanem helyben irom meg az egeszet:
-//a kovetkezo fuggveny az element-el kezdve egy call back fuggveny ami nincs eléore gyartva hanem inline lett megadva. Akkor fut le amikor a forEach mondja nekii
-arr.forEach(element => console.log(element * 2))
+studentsData.forEach(student => {
+    console.log(student.name)
+    
+    student.subjects.forEach(subject => console.log(`${subject.subjectName}: ${subject.subjectGrade}`))
+})
